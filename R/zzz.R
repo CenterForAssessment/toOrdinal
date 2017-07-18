a94d6191dc9ca5865bad91fec6f1c33688ab3ceb
dbetebenner <dbetebenner@gmail.com>
2017-07-18 12:14:46 -0400

    Updating DESCRIPTION to pass CRAN checks

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-29-2017). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal'))
+		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (7-18-2017). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal'))
 	}
 }
