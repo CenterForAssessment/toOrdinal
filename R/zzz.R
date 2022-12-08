df02ea042e2f4f9ae7b28e000619271ccededeaa
Damian Betebenner <dbetebenner@nciea.org>
2022-12-07 22:32:52 -0500

    Moving testthat to suggests

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (2-18-2022). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal/'))
+		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-4-2022). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal/'))
 	}
 }
