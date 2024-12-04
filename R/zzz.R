b7919d8f6410c8668dc8ad0b49343eb94b6c3d44
Damian Betebenner <dbetebenner@nciea.org>
2024-12-03 16:25:55 -1000

    Updating startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,20 @@
 function(libname, pkgname) {
 }
 
-`.onAttach` <-
-function(libname, pkgname) {
-	if (interactive()) {
-		packageStartupMessage(magenta$bold('toOrdinal',paste(paste0(unlist(strsplit(as.character(packageVersion("toOrdinal")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-4-2022). For help: >help("toOrdinal") or visit https://centerforassessment.github.io/toOrdinal/'))
-	}
+.onAttach <- function(libname, pkgname) {
+    if (interactive()) {
+        # Extract version information
+        version <- utils::packageVersion("toOrdinal")
+
+        # Define a friendly startup message
+	message_text <- paste0(
+	    magenta(bold("\uD83C\uDF89 toOrdinal v", version)), " - ", toOrdinal::toOrdinalDate("2024-12-3"), "\n",
+	    "\U1F4A1 Tip: ", magenta(bold("> help(\"toOrdinal\")")), "\n",
+	    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/toOrdinal")), "\n",
+	    "\u2728 Happy toOrdinaling!"
+	)
+
+        # Display the startup message
+        packageStartupMessage(message_text)
+    }
 }
